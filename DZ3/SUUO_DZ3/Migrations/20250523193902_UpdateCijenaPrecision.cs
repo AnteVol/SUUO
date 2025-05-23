baf2627ff5140d4159040f40ace64a8033642982
AnteVol <ante.volarevic@fer.hr>
2025-05-23 21:48:25 +0200

    New migration

---
+++ DZ3/SUUO_DZ3/Migrations/20250523193902_UpdateCijenaPrecision.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SUUO_DZ3.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCijenaPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MetodaPlacanja",
+                table: "Narudzbe",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MetodaPlacanja",
+                table: "Narudzbe",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
