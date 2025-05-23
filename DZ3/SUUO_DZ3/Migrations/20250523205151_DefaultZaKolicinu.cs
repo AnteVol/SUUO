1f3fff5bae9de70748312533462f8fa958727a38
AnteVol <ante.volarevic@fer.hr>
2025-05-23 22:52:43 +0200

    Migracije

---
+++ DZ3/SUUO_DZ3/Migrations/20250523205151_DefaultZaKolicinu.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SUUO_DZ3.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefaultZaKolicinu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
